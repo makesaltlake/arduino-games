7f0ee0ba91a9a593502914d39193f89973ec339f
Duane Johnson <duane.johnson@gmail.com>
2013-09-25 10:03:44 -0600

    Add complete (cheat) version of etchasketch

--- etchasketch/etchasketch.ino
+++ etchasketch/etchasketch.ino
@@ -1,21 +1,3 @@
-/***************************************************
-  This is an example sketch for the Adafruit 1.8" SPI display.
-  This library works with the Adafruit 1.8" TFT Breakout w/SD card
-  ----> http://www.adafruit.com/products/358
-  as well as Adafruit raw 1.8" TFT display
-  ----> http://www.adafruit.com/products/618
- 
-  Check out the links above for our tutorials and wiring diagrams
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional)
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
-
 #define paddle_l 22
 #define paddle_r 21
 //#define sclk 13
@@ -29,7 +11,6 @@
 #include <SPI.h>
 
 Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
-float p = 3.1415926;
 
 void setup(void) {
   tft.initR(INITR_REDTAB);   // alternative: INITR_GREENTAB
---
+++ etchasketch/etchasketch_cheat.ino
@@ -0,0 +1,92 @@
+const int pinLightL = 3;
+const int pinLightM = 4;
+const int pinLightR = 5;
+
+const int pinButtonL = 22;
+const int pinButtonM = 21;
+const int pinButtonR = 20;
+
+//// Define pins used by potentiometer knobs
+//#define paddle_l 22
+//#define paddle_r 21
+//
+//// Define pins used by buttons
+//#define button_l 4
+//#define button_r 5
+//#define button_m 6
+
+
+// Define pins used by potentiometer knobs
+#define paddle_l 16
+#define paddle_r 15
+
+// Define pins used by buttons
+#define button_l 22
+#define button_r 21
+#define button_m 20
+
+// Define pins used by SPI to communicate with LCD display
+#define cs   10
+#define dc   9
+#define rst  8  // you can also connect this to the Arduino reset
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+
+// Initialize the display object with the pins we chose
+Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
+
+void setup(void) {
+  pinMode(button_l, INPUT);
+  pinMode(button_r, INPUT);
+  pinMode(button_m, INPUT);
+  
+  tft.initR(INITR_REDTAB);   // alternative: INITR_GREENTAB
+  tft.fillScreen(ST7735_BLACK);
+  delay(500);
+}
+
+#define PADDLE_MAX 1024
+#define WIDTH 126
+#define HEIGHT 158
+
+int x, y, color;
+int color_cycle = 0;
+
+int get_color(int cycle_value) {
+  switch(cycle_value % 5) {
+    case 0: return ST7735_BLUE;
+    case 1: return ST7735_GREEN;
+    case 2: return ST7735_CYAN;
+    case 3: return ST7735_MAGENTA;
+    case 4: return ST7735_YELLOW;
+  }
+}
+
+void loop() {
+  x = analogRead(paddle_l) * WIDTH / PADDLE_MAX;
+  y = analogRead(paddle_r) * HEIGHT / PADDLE_MAX;
+  
+  if (digitalRead(button_l) == HIGH) {
+    color_cycle++;
+    delay(400);
+  }
+  
+  color = get_color(color_cycle);
+  
+  if (digitalRead(button_r) == HIGH) {
+    tft.fillScreen(color);
+    color_cycle++;
+    delay(400);
+  } else {
+    tft.drawPixel(WIDTH-x, HEIGHT-y, color);
+  }
+
+  if (digitalRead(button_m) == HIGH) {
+    tft.fillScreen(ST7735_BLACK);
+  }
+  
+  
+}
+
